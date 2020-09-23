5b2ad038ecfee452f87f9c5d8fae1238da703c67
Melissa Poulsen <melissakatie@icloud.com>
2020-09-23 09:17:24 -0600

    Starting SwiftUI

---
+++ Day16/Day16/Day16/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day16
+//
+//  Created by Melissa Poulsen on 9/23/20.
+//  Copyright © 2020 Melissa Poulsen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
